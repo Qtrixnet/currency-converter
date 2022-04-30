2f3010fc6cb074936e7944b54e3c8cad437f42f3
Kirill <qtrixnet@yandex.ru>
2022-04-30 14:15:19 +0600

    добавить зависимостть

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,6 +1,5 @@
 import {rootReducer} from './reducers';
 import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux'
 
 const store = configureStore({
   reducer: rootReducer,
